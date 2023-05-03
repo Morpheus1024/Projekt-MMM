2eea1ef73942aaf2c99ecff4fb0bb702ca0a958f
Mateusz Drzewiecki <105047709+drzewo1337@users.noreply.github.com>
2023-05-03 19:16:52 +0200

    Update projekt2104.py

--- projekt2104.py
+++ projekt2104.py
@@ -4,12 +4,15 @@ import numpy as np
 from numpy import exp as exp
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
-
+from matplotlib.widgets import RadioButtons
 
 class Uklad:
 
     def __init__(self, lista_IN):
-        self.gs = None
+        self.IN = None
+        self.amp_slider = Slider
+        self.axamp = None
+        self.line1 = None
         self.R = lista_IN[0]
         self.L = lista_IN[1]
         self.R2 = lista_IN[2]
@@ -18,50 +21,44 @@ class Uklad:
         self.biegun = None
         self.Y_string = ''
         self.transmitancja = ' Y/U = -R/(R+R2+sRCR2)'
+        fig = plt.figure(figsize=[10, 7])
+        self.fig = fig
+        fig.subplots_adjust(left=0.2, bottom=0.2)
+        gs = fig.add_gridspec(2, 2)
+        self.gs = gs
+
+    def rysowanko(self, IN, time, dt):
+        t = np.arange(0, time, dt)
+        ax3 = self.fig.add_subplot(self.gs[0, 1])
+        line1 = plt.plot(t, self.mnozenie_plot(IN, amp_slider.val))
 
     def bode(self):
         self.sys = signal.TransferFunction([self.R], [self.R * self.R2 * self.C, self.R + self.R2])
         w, mag, phase = signal.bode(self.sys)
-        self.fig = plt.figure()
-        fig = self.fig
-        fig.subplots_adjust(left=0.2, bottom = 0.2)
-        gs = fig.add_gridspec(2, 2)
-        self.gs = gs
-        ax1 = fig.add_subplot(gs[0, 0])
+        ax1 = self.fig.add_subplot(self.gs[0, 0])
         plt.semilogx(w, phase)
-        ax2 = fig.add_subplot(gs[1, 0])
+        ax2 = self.fig.add_subplot(self.gs[1, 0])
         plt.semilogx(w, mag)
 
-
-    def amplitude(self, A):
-        amplitude1 = self.fig.add_axes([0.1, 0.25, 0.0225, 0.63])  # left, bottom, width, lenght)
-        amp_slider = Slider(
-            ax=amplitude1,
-            label="Amplitude",
-            valmin=0,
-            valmax=10,
-            valinit=A,
-            orientation="vertical"
-        )
-
-    def update(self, val, t):
-        ax3 = self.fig.add_subplot(self.gs[0, 1])
-        plt.plot(t, IN*A)
-
+    def mnozenie_plot(self, IN, A):
+        wynik = IN*A
+        return wynik
 
 
     def sygnal(self, time, A, w, dt):
         t = np.arange(0, time, dt)
         IN1 = signal.square(w * t)
+        self.IN1 = IN1
         IN2 = signal.sawtooth(w * t, 0.5)
+        self.IN2 = IN2
         IN3 = np.sin(t)
-        IN = IN3
+        self.IN3 = IN3
         ax3 = self.fig.add_subplot(self.gs[0, 1])
-        plt.plot(t, IN*A)
         ax3.set_title('IN')
+        IN = IN1
+        self.IN = IN
 
         return IN
-
     def wyjscie(self, time, IN, dt):
         t = np.arange(0, time, dt)
         OUT = IN * ((self.R * exp(-(t * (self.R + self.R2)) / (self.C * self.R * self.R2))) / (
@@ -81,10 +78,20 @@ dt = 0.1
 A = 2
 w = 1
 uklad = Uklad(RLC)
-
 uklad.bode()
 IN = uklad.sygnal(time, A, w, dt)
+uklad.rysowanko(IN, time, dt)
+axamp = uklad.fig.add_axes([0.1, 0.25, 0.0225, 0.63])
+amp_slider = Slider(
+        ax=axamp,
+        label="Amplitude",
+        valmin=0,
+        valmax=10,
+        valinit=A,
+        orientation="vertical"
+    )
+amp_slider.on_changed(uklad.rysowanko(IN, time, dt))
 uklad.wyjscie(time, IN, dt)
-uklad.amplitude(A)
+uklad.mnozenie_plot(IN, A)
 plt.show()
 ###################################################
