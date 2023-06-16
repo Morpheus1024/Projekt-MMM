bf75d0fa27b6142bf1818f5dd48d54556bc2084c
Mikołaj Galant <mikolaj.galant@gmail.com>
2023-06-16 19:28:45 +0200

    dodanie interfaceu do prawie gotowe

--- mmm16.py
+++ mmm16.py
@@ -5,16 +5,6 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import TextBox, Button
 
 
-
-
-
-
-
-
-
-
-
-
 #wykresy
 fig, axs = plt.subplots(2, 2)
 fig.subplots_adjust(left=0.08, bottom=0.30, right=0.98, top=0.99, wspace=0.25, hspace=0.25)
@@ -32,43 +22,54 @@ axs[1, 1].set_ylabel('U_OUT [V]')
 axs[1, 1].set_xlabel('t [s]')
 
 
+
+
 wartosci_BODE = [10, 15, 1, 20]
 wartosci_IN = [2, 100, 100, 1]
 
 axbox = fig.add_axes([0.07, 0.06, 0.10, 0.075])
 amp_box = TextBox(axbox, "A [V] ", textalignment="center")
-amp_box.set_val(wartosci_IN[0])
+amp_box.set_val(2)
+amp_box.on_submit(lambda val: wart_A(val))
+
 
 timeax = fig.add_axes([0.32, 0.06, 0.10, 0.075])
 time_box = TextBox(timeax, "time [s] ", textalignment="center")
-time_box.set_val(wartosci_IN[1])
+time_box.set_val(100)  # Trigger `submit` with the initial string.
+time_box.on_submit(lambda val: wart_time(val))
 
 dtax = fig.add_axes([0.57, 0.06, 0.10, 0.075])
 dt_box = TextBox(dtax, "dt [ms] ", textalignment="center")
-dt_box.set_val(wartosci_IN[2])
+dt_box.set_val(0.1)  # Trigger `submit` with the initial string.
+dt_box.on_submit(lambda val: wart_dt(val))
 
 wax = fig.add_axes([0.80, 0.06, 0.10, 0.075])
 w_box = TextBox(wax, "ω [Hz] ", textalignment="center")
-w_box.set_val(wartosci_IN[3])
+w_box.set_val(1)  # Trigger `submit` with the initial string.
+w_box.on_submit(lambda val: wart_w(val))
 
 
 
 
 Rax = fig.add_axes([0.07, 0.15, 0.10, 0.075])
 R_box = TextBox(Rax, "R [Ω]", textalignment="center")
-R_box.set_val(wartosci_BODE[0])
+R_box.set_val(10)  # Trigger `submit` with the initial string.
+R_box.on_submit(lambda val: wart_R(val))
 
 R2ax = fig.add_axes([0.32, 0.15, 0.10, 0.075])
 R2_box = TextBox(R2ax, "R2 [Ω]", textalignment="center")
-R2_box.set_val(wartosci_BODE[1])
+R2_box.set_val(15)  # Trigger `submit` with the initial string.
+R2_box.on_submit(lambda val: wart_R2(val))
 
 Lax = fig.add_axes([0.57, 0.15, 0.10, 0.075])
 L_box = TextBox(Lax, "L [uH]", textalignment="center")
-L_box.set_val(wartosci_BODE[2])
+L_box.set_val(1)  # Trigger `submit` with the initial string.
+L_box.on_submit(lambda val: wart_L(val))
 
 Cax = fig.add_axes([0.80, 0.15, 0.10, 0.075])
 C_box = TextBox(Cax, "C [uF]", textalignment="center")
-C_box.set_val(wartosci_BODE[3])
+C_box.set_val(20)  # Trigger `submit` with the initial string.
+C_box.on_submit(lambda val: wart_C(val))
 
 
 
--- projekt_prawie_gotowy.py
+++ projekt_prawie_gotowy.py
@@ -30,7 +30,8 @@ def f_IN3(A, w, time, dt):
 
 
 fig, axs = plt.subplots(2, 2)
-fig.subplots_adjust(left=0.3, bottom=0.2)
+fig.subplots_adjust(left=0.08, bottom=0.30, right=0.98, top=0.99, wspace=0.25, hspace=0.25)
+
 
 
 def f_sys(R, R2, C):
@@ -38,12 +39,17 @@ def f_sys(R, R2, C):
     w1, mag, phase = signal.bode(sys)
     return w1, mag, phase
 
-
-
 axs[0, 0].set_ylabel('Phase')
-axs[1, 0].set_xlabel('Mag')
-axs[0, 1].set_ylabel('IN')
-axs[1, 1].set_xlabel('OUT')
+axs[0, 0].set_xlabel('f')
+
+axs[0, 1].set_ylabel('U_IN [V]')
+axs[0, 1].set_xlabel('t [s]')
+
+axs[1, 0].set_ylabel('Mag')
+axs[1, 0].set_xlabel('f')
+
+axs[1, 1].set_ylabel('U_OUT [V]')
+axs[1, 1].set_xlabel('t [s]')
 
 wartosci_BODE = [10, 1, 15, 20]
 wartosci_IN = [2, 100, 0.1, 1]
@@ -143,53 +149,59 @@ def rysowanie_plotow1(f):
     plt.show()
 
 
-axbox = fig.add_axes([0.13, 0.02, 0.15, 0.075])
-amp_box = TextBox(axbox, "amplitude", textalignment="center")
-amp_box.set_val(2)  # Trigger `submit` with the initial string.
+axbox = fig.add_axes([0.07, 0.06, 0.10, 0.075])
+amp_box = TextBox(axbox, "A [V] ", textalignment="center")
+amp_box.set_val(2)
 amp_box.on_submit(lambda val: wart_A(val))
 
-timeax = fig.add_axes([0.35, 0.02, 0.15, 0.075])
-time_box = TextBox(timeax, "time", textalignment="center")
+
+timeax = fig.add_axes([0.32, 0.06, 0.10, 0.075])
+time_box = TextBox(timeax, "time [s] ", textalignment="center")
 time_box.set_val(100)  # Trigger `submit` with the initial string.
 time_box.on_submit(lambda val: wart_time(val))
 
-dtax = fig.add_axes([0.55, 0.02, 0.15, 0.075])
-dt_box = TextBox(dtax, "dt", textalignment="center")
+dtax = fig.add_axes([0.57, 0.06, 0.10, 0.075])
+dt_box = TextBox(dtax, "dt [ms] ", textalignment="center")
 dt_box.set_val(0.1)  # Trigger `submit` with the initial string.
 dt_box.on_submit(lambda val: wart_dt(val))
 
-wax = fig.add_axes([0.75, 0.02, 0.15, 0.075])
-w_box = TextBox(wax, "w", textalignment="center")
+wax = fig.add_axes([0.80, 0.06, 0.10, 0.075])
+w_box = TextBox(wax, "ω [Hz] ", textalignment="center")
 w_box.set_val(1)  # Trigger `submit` with the initial string.
 w_box.on_submit(lambda val: wart_w(val))
 
-Rax = fig.add_axes([0.05, 0.3, 0.15, 0.075])
-R_box = TextBox(Rax, "R", textalignment="center")
+
+
+
+Rax = fig.add_axes([0.07, 0.15, 0.10, 0.075])
+R_box = TextBox(Rax, "R [Ω]", textalignment="center")
 R_box.set_val(10)  # Trigger `submit` with the initial string.
 R_box.on_submit(lambda val: wart_R(val))
 
-Lax = fig.add_axes([0.05, 0.4, 0.15, 0.075])
-L_box = TextBox(Lax, "L", textalignment="center")
-L_box.set_val(1)  # Trigger `submit` with the initial string.
-L_box.on_submit(lambda val: wart_L(val))
-
-R2ax = fig.add_axes([0.05, 0.5, 0.15, 0.075])
-R2_box = TextBox(R2ax, "R2", textalignment="center")
+R2ax = fig.add_axes([0.32, 0.15, 0.10, 0.075])
+R2_box = TextBox(R2ax, "R2 [Ω]", textalignment="center")
 R2_box.set_val(15)  # Trigger `submit` with the initial string.
 R2_box.on_submit(lambda val: wart_R2(val))
 
-Cax = fig.add_axes([0.05, 0.2, 0.15, 0.075])
-C_box = TextBox(Cax, "C", textalignment="center")
+Lax = fig.add_axes([0.57, 0.15, 0.10, 0.075])
+L_box = TextBox(Lax, "L [uH]", textalignment="center")
+L_box.set_val(1)  # Trigger `submit` with the initial string.
+L_box.on_submit(lambda val: wart_L(val))
+
+Cax = fig.add_axes([0.80, 0.15, 0.10, 0.075])
+C_box = TextBox(Cax, "C [uF]", textalignment="center")
 C_box.set_val(20)  # Trigger `submit` with the initial string.
 C_box.on_submit(lambda val: wart_C(val))
 
-IN1_butt_box=fig.add_axes([0.03, 0.65, 0.18, 0.05])
+
+
+IN1_butt_box=fig.add_axes([0.0625, 0.01, 0.25, 0.04])
 IN1_button = Button(IN1_butt_box,'Syg. prosto.')
 
-IN2_butt_box=fig.add_axes([0.03, 0.75, 0.18, 0.05])
+IN2_butt_box=fig.add_axes([0.375, 0.01, 0.25, 0.04])
 IN2_button = Button(IN2_butt_box,'Syg. trójkąt.')
 
-IN3_butt_box=fig.add_axes([0.03, 0.85, 0.18, 0.05])
+IN3_butt_box=fig.add_axes([0.6875, 0.01, 0.25, 0.04])
 IN3_button = Button(IN3_butt_box,'Syg. harmon,')
 
 IN1_button.on_clicked(on_IN1_button_clicked)
